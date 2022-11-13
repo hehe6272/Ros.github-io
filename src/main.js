579a851b5238044a0d824c008741a1bdf8b6aa34
mmm25002500 <marryhan25648@gmail.com>
2022-11-13 19:07:28 +0800

    新增 README 和 LICENCE

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+//  Written by Tershixia for this PROJECT.
+//  Copyright (C) 2022 TershiXia
+//  This file is distributed under the same license as the PROJECT project.
+//  mmm25002500 <admin@tershi.com>, 2022.
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers';
--- src/views/YoutubePage.vue
+++ src/views/YoutubePage.vue
@@ -1,3 +1,10 @@
+<!--
+  Written by Tershixia for this PROJECT.
+  Copyright (C) 2022 TershiXia
+  This file is distributed under the same license as the PROJECT project.
+  mmm25002500 <admin@tershi.com>, 2022.
+ -->
+
 <template>
   hi
 </template>
